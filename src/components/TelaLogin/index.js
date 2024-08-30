826995314de3872684e0c6ee7a9d4f4dd5150718
vagnerme <vagnermelo3005@gmail.com>
2024-08-30 19:39:57 -0300

    atualização

--- src/components/TelaLogin/index.js
+++ src/components/TelaLogin/index.js
@@ -44,6 +44,7 @@ function TelaLogin() {
 
     return (<ThemeProvider theme={defaultTheme}>
         <Container component="main" maxWidth="xs">
+<<<<<<< Updated upstream
             <CssBaseline />
             <Box
                 sx={{
@@ -105,6 +106,72 @@ function TelaLogin() {
                         </Grid>
                     </Grid>
                 </Box>
+=======
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main'}}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+            
+
+             
+             Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email "
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="lembrar me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                ENTRAR
+              </Button>
+
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Esqueceu sua senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Não tem uma conta? Inscrever-se"}
+                  </Link>
+                </Grid>
+              </Grid>
+>>>>>>> Stashed changes
             </Box>
             <Copyright sx={{ mt: 8, mb: 4 }} />
         </Container>
