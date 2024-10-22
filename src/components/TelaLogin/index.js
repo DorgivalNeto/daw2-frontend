c538495128e2c916a281eea343a23f3e895ceb61
DorgivalNeto <dorgival.neto@academico.ifpb.edu.br>
2024-10-22 20:14:30 -0300

    autenticação 2

--- src/components/TelaLogin/index.js
+++ src/components/TelaLogin/index.js
@@ -1,33 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Logo from '../../assets/images/logo.png';
 import { useNavigate } from "react-router-dom";
-import {useStorege } from "../../security/Authprovider"
-import api from "../../services/api";
 
 
 
-const Copyright = (props) => {
-
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
 
 const defaultTheme = createTheme();
 
@@ -37,36 +21,7 @@ const defaultTheme = createTheme();
 function TelaLogin() {
 
     const navigate = useNavigate();
-    const { login } = useStorege();
-
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-
-    const fazerLogin = () => {
-
-        if (email.trim() != "" && senha.trim() != "") {
-            api.login(
-
-                {
-                    identifier: email,
-                    password: senha,
-                }
-            ).
-            then((response) => {
-                login (response.data.jwt)
-                navigate("/inicio")
-
-                
-     }).catch((error) => { 
-        console.log(error)
-     })
-        }
-
-
-        login();
-        navigate("/servico")
-    }
-
+    
     return (
 
         <ThemeProvider theme={defaultTheme}>
@@ -134,7 +89,7 @@ function TelaLogin() {
                                 type="submit"
                                 fullWidth
                                 variant="contained"
-                                onClick={() => { fazerLogin() }}
+                                onClick={() => { navigate("/servico") }}
                                 sx={{ mt: 3, mb: 2 }}
                             >
                                 Entrar
