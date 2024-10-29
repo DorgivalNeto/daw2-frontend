7ddee803ae885673e570d570387dbb986c5b9827
DorgivalNeto <dorgival.neto@academico.ifpb.edu.br>
2024-10-29 20:02:30 -0300

    remoção da autenticação

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import Routes from './routes';
-import StoreProvider from './security/Authprovider/provider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <StoreProvider>
+      
       <Routes/>
-      </StoreProvider>
+      
     </React.StrictMode>
   </BrowserRouter>
 );
--- src/routes/Private/index.js
+++ src/routes/Private/index.js
@@ -1,12 +1,12 @@
 
 import PaginaServico from "../../pages/PaginaServico";
-import ProtectedRoute from "../../security/ProtectedRoute";
+
 
 const Private ={ 
     
         
             path: '/servico',
-            element: <ProtectedRoute element={ <PaginaServico /> } />
+            element: <PaginaServico/>
         
 
         
